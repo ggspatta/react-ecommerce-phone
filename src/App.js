0902df1c90063a48fec1493cff367478a2556ee7
Gerhard Spatta <ggspatta@presby.edu>
2020-05-21 20:18:06 -0400

    Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Switch, Route } from 'react-router-dom'
 import logo from "./logo.svg";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import Default from './components/Default'
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+import Details from './components/Details'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>hello from app</h3>
+        <Navbar />
+        <Switch>
+          <Route path='/' component={ProductList}></Route>
+        </Switch>
+        
+        <Details />
+        <Cart />
+        <Default />
       </React.Fragment>
     );
   }
