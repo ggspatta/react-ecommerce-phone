45592697eb8e19f2cab31286acc18d51b4844ae9
Gerhard Spatta <ggspatta@presby.edu>
2020-05-23 14:30:23 -0400

    Add context

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
-import Product from './Product';
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import {storeProducts} from "../data"
+import {ProductConsumer} from '../context'
 
 export default class ProductList extends Component {
-    render() {
-        return (
-            <div>
-               <Product />
+  state = {
+    products: storeProducts
+  };
+  render() {
+      console.log(this.state.products);
+      
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+                <ProductConsumer>
+                    {(value)=>{
+                        return <h1>
+                            {value}
+                        </h1>
+
+                    }
+                    
+                    }
+                </ProductConsumer>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import "./index.css";
 import App from "./App";
+import { ProductProvider } from "./context";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </ProductProvider>,
   document.getElementById("root")
 );
 
