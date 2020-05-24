fd9627a4635c553c164bda569a8e67ad12573855
Gerhard Spatta <ggspatta@presby.edu>
2020-05-24 11:47:50 -0400

    context update

--- src/context.js
+++ src/context.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react'
-
-
+import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 const ProductContext = React.createContext();
 
-
 class ProductProvider extends Component {
-    render() {
-        return (
-            <ProductContext.Provider value="hello from context">
-                { this.props.children }
-            </ProductContext.Provider>
-        )
-    }
+  state = {
+    products: [],
+    detailProduct: detailProduct,
+  };
+
+  componentDidMount() {
+    this.setProducts();
+    
+  }
+  /* Copy products into fresh objext since passed as reference.  De-reference it */
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];      
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
+  handleDetail = () => {
+    console.log("hello from detials");
+  };
+
+  addToCart = () => {
+    console.log("Hello from add to cart");
+  };
+
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart,
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
-export { ProductProvider, ProductConsumer }
+export { ProductProvider, ProductConsumer };
